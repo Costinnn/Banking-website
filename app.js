582ddc8d149ee7795b90d63a3774bc20940aeb45
Costinnn <crauciuc.costin@yahoo.com>
2022-09-03 12:32:30 +0300

    content added

--- app.js
+++ app.js
@@ -1,35 +1,32 @@
-
 // Variables for slides buttons
-const rightBtn = document.querySelector('.right-arrow');
-const leftBtn = document.querySelector('.left-arrow');
-const slides = document.querySelector('.slides');
+const rightBtn = document.querySelector(".right-arrow");
+const leftBtn = document.querySelector(".left-arrow");
+const slides = document.querySelector(".slides");
 
 // Variables for slides icons
-const iconsText = document.getElementsByClassName('icons-text');
-const icon = document.getElementsByClassName('icon');
+const iconsText = document.getElementsByClassName("icons-text");
+const icon = document.getElementsByClassName("icon");
 
 // Slides buttons functions
-rightBtn.addEventListener('click', () => {
-    
-    if(window.innerWidth > 1440){
-        slides.style.marginLeft = '-1440px';
-    } else {
-        slides.style.marginLeft = '-100vw'
-    }
-    
-})
+rightBtn.addEventListener("click", () => {
+  if (window.innerWidth > 1440) {
+    slides.style.marginLeft = "-1440px";
+  } else {
+    slides.style.marginLeft = "-100vw";
+  }
+});
 
-leftBtn.addEventListener('click', () => {
-    slides.style.marginLeft = '0'
-})
+leftBtn.addEventListener("click", () => {
+  slides.style.marginLeft = "0";
+});
 
 //Slides icons functions
 
-for(let i = 0; i< icon.length; i++){
-    icon[i].addEventListener('mouseenter',() => {
-       iconsText[i].style.display = 'block';
-    })
-    icon[i].addEventListener('mouseleave',() => {
-        iconsText[i].style.display = 'none';
-     })
+for (let i = 0; i < icon.length; i++) {
+  icon[i].addEventListener("mouseenter", () => {
+    iconsText[i].style.display = "block";
+  });
+  icon[i].addEventListener("mouseleave", () => {
+    iconsText[i].style.display = "none";
+  });
 }
